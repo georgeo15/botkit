909e6f2406dc75980f29294049352a0deacd5a67
george15 <bumfuzzlers@gmail.com>
2016-02-23 11:08:55 +0200

    ADDED NEW CONROLLER

--- bot.js
+++ bot.js
@@ -120,6 +120,22 @@ controller.hears(['call me (.*)'],'direct_message,direct_mention,mention',functi
         });
     });
 });
+/******************MY CHANGE HERE*****************/
+controller.hears(['call botgeorge(.*)'],'direct_message,direct_mention,mention',function(bot, message) {
+    var matches = message.text.match(/call me (.*)/i);
+    var name = matches[1];
+    controller.storage.users.get(message.user,function(err, user) {
+        if (!user) {
+            user = {
+                id: message.user,
+            };
+        }
+        user.name = name;
+        controller.storage.users.save(user,function(err, id) {
+            bot.reply(message,'Got it. I will call you ' + user.name + ' from now on.');
+        });
+    });
+});
 
 controller.hears(['what is my name','who am i'],'direct_message,direct_mention,mention',function(bot, message) {
 
